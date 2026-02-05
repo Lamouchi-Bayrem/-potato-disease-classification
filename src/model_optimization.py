ba49d5bd8f7a5647be377b32232bd4eb6d03f58b
L-Bayrem <160652712+Lamouchi-Bayrem@users.noreply.github.com>
2026-02-05 20:11:13 +0100

    Add model compilation function for TensorFlow

---
+++ src/model_optimization.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+def compile_model(model):
+    model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+        metrics=['accuracy']
+    )
+    return model
