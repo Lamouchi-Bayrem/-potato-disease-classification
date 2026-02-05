e029c8ebb5464ad081dc780b0fd33572b01c3f82
L-Bayrem <160652712+Lamouchi-Bayrem@users.noreply.github.com>
2026-02-05 20:18:30 +0100

    Update constants.py

---
+++ src/constants.py
@@ -0,0 +1,6 @@
+BATCH_SIZE = 32
+IMAGE_SIZE = 256
+CHANNELS = 3
+EPOCHS = 50
+DATA_DIR = "../data/PlantVillage"  # Adjust path to dataset
+CLASS_NAMES = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
