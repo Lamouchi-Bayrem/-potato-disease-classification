1b911d262ef65a85466f3667e6a06d3f853956eb
L-Bayrem <160652712+Lamouchi-Bayrem@users.noreply.github.com>
2026-02-06 22:05:55 +0100

    Implement dataset download and model directory setup
    Added dataset downloading and directory creation.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import os
+
+from data_download import download_dataset
 from data_loading import load_dataset
 from dataset_partition import get_dataset_partitions_tf
 from model_building import build_model
@@ -10,6 +13,13 @@ from tflite_conversion import convert_to_tflite, convert_with_quantization
 from qat import train_qat_model  # Optional: Uncomment for QAT
 
 def main():
+    # Create directories if not exist
+    if not os.path.exists('../models'):
+        os.makedirs('../models')
+    
+    # Download dataset if not present
+    download_dataset()
+    
     dataset, class_names = load_dataset()
     train_ds, val_ds, test_ds = get_dataset_partitions_tf(dataset)
     
@@ -26,12 +36,12 @@ def main():
     save_model(model)
     save_h5_model(model)
     
-    convert_to_tflite(model, "models/model.tflite")
-    convert_with_quantization(model, train_ds, "models/model_quant.tflite")
+    convert_to_tflite(model, "../models/model.tflite")
+    convert_with_quantization(model, train_ds, "../models/model_quant.tflite")
     
     # Optional: QAT (Quantization Aware Training)
     # q_aware_model, q_history = train_qat_model(train_ds, val_ds)
-    # convert_to_tflite(q_aware_model, "models/model_qat.tflite")
+    # convert_to_tflite(q_aware_model, "../models/model_qat.tflite")
 
 if __name__ == "__main__":
     main()
